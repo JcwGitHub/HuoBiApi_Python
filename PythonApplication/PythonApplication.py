8ef4d4a09925a32f15955643c01d4ae6bf13501e
MR.J <772763832@qq.com>
2019-10-11 00:56:09 +0800

    修改为python

---
+++ PythonApplication/PythonApplication.py
@@ -0,0 +1,27 @@
+﻿
+import json
+
+from HuobiDMService import HuobiDM
+from pprint import pprint
+
+#### input huobi dm url
+URL = ''
+ACCESS_KEY = ''
+SECRET_KEY = ''
+
+####  input your access_key and secret_key below:
+with open('C:\HuoBiKey.json','r') as loadf :
+    load_dict = json.load(loadf)
+    URL = load_dict['URL']
+    ACCESS_KEY = load_dict['Access_Key']
+    SECRET_KEY = load_dict['Secret_Key']
+    pprint(URL)
+    pprint(ACCESS_KEY)
+    pprint(SECRET_KEY)
+
+#初始化账号
+dm = HuobiDM(URL, ACCESS_KEY, SECRET_KEY)
+
+print (u' 获取用户账户信息 ')
+pprint (dm.get_contract_account_info())
+pprint (dm.get_contract_account_info("EOS"))
